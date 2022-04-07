dfdd4cc5e3092ff1186c78f65aae675769ba06a1
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-04-07 23:49:43 +0800

    added time, and timer for monday 4am

---
+++ Clock/app.js
@@ -0,0 +1,33 @@
+const countdown = ()=>{
+
+    function getResetDay() {
+        var now = new Date();
+        var ResetDay = new Date();
+        ResetDay.setDate(now.getDate() + (8 - 1 - now.getDay() + 7) % 7 + 1); //Monday
+        ResetDay.setHours(4, 0, 0, 0); //4:00 AM
+        return ResetDay;
+    }
+    const now = new Date().getTime();
+    const deadLine = new Date(getResetDay()).getTime();
+    const gap = deadLine - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('.day').innerText = textDay;
+    document.querySelector('.hour').innerText = textHour;
+    document.querySelector('.minute').innerText = textMinute;
+    document.querySelector('.second').innerText = textSecond;
+}
+
+if (countdown().gap < 0){
+    countdown();
+}
+setInterval(setInterval(countdown,1000),604800);
