d413690e8062f353338b9677652610bd37d29ade
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-09 22:44:36 +0800

    added app-routing for character and weapon views

---
+++ genshinToDo/src/app/character-view/character-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-view',
+  template: `<p>characterView</p>`,
+  styles: [``]
+})
+export class CharacterViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ genshinToDo/src/app/weapon-view/weapon-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weapon-view',
+  template: `
+    <p>
+      weapon-view works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WeaponViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
