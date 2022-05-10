442303a33172adb8e13b83048d34670824b5d7d0
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-10 20:28:59 +0800

    added ts file exclusive for interfaces only

---
+++ genshinToDo/src/app/interfaces.ts
@@ -0,0 +1,20 @@
+export interface characterInterface {
+    star:Number,
+    name:String,
+    weaponType:String,
+    element:String,
+    region:String,  
+    domain:String,
+    material:String,
+    availability:String[]
+}
+
+export interface weaponInterface{
+    star:Number,
+    name:String,
+    secondaryStat:String
+    weaponType:String,
+    domain:String,
+    material:String,
+    availability:String[]
+}
