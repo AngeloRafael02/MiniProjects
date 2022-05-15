27376b1377254c70dca1e5a0063030c1e63dcd9d
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-15 22:07:16 +0800

    made the Student Class work

--- PythonPOS/POS_SiglePage_V2.py
+++ PythonPOS/POS_SiglePage_V2.py
@@ -2,16 +2,26 @@
 # Made by Angelo Rafael Recio
 
 class Student():
-    def __init__(self,name,id,year,course,section,budget):
+    def __init__(self,name:str,id:str,year:str,course:str,section:str):
         self.name = name
         self.id = id
         self.year = year
         self.course = course
         self.section = section
-        self.budget = budget
-    @staticmethod
-    def spend(wallet, price):
-        return wallet - price
+
+while True:
+    try:
+        name251:str = str(input("Input Name Here: "))
+        id251:str = str(input("Input Id Here: "))
+        year251:str = str(input("Input Year Here: "))
+        course251:str = str(input("Input Course Here: "))
+        section251:str = str(input("Input Section Here: ")) 
+        break
+    except Exception as error:
+        print(error)
+
+USER = Student(name251,id251,year251,course251,section251)
+
 
     
 
