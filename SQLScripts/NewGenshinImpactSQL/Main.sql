41d640471060f8170689b0dd1aec6f41d07a5ef2
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-27 23:20:25 +0800

    added command to run createViews.sql

--- SQLScripts/NewGenshinImpactSQL/Main.sql
+++ SQLScripts/NewGenshinImpactSQL/Main.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE genshin_impact;
-
+\c genshin_impact
 \set ON_ERROR_STOP 1
 begin;
-    \i tables.sql
-    \i SideTable.sql
-    \i CharactersData.sql
-    (Weapons Table Here)
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/tables.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/SideTable.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CharactersData.sql
+    
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/createViews.sql
 commit;
