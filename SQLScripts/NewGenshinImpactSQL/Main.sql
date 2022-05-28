de1ce51a45657e9c90e6e95c946ca7009305ae08
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-28 23:06:05 +0800

    Added new weapons data file

--- SQLScripts/NewGenshinImpactSQL/Main.sql
+++ SQLScripts/NewGenshinImpactSQL/Main.sql
@@ -2,9 +2,9 @@ CREATE DATABASE genshin_impact;
 \c genshin_impact
 \set ON_ERROR_STOP 1
 begin;
-    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/tables.sql
-    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/SideTable.sql
-    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CharactersData.sql
-    
-    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/createViews.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateAllTables.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/InsertSideTableData.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateCharactersData.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateWeaponsData.sql
+    \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateViews.sql
 commit;
