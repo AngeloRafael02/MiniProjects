882583ba07d5fa777d9dd1c8c0adf52cf54599eb
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-05-30 22:39:11 +0800

    improved  Data display

--- genshinToDo/src/app/character-details/character-details.component.ts
+++ genshinToDo/src/app/character-details/character-details.component.ts
@@ -7,11 +7,12 @@ import { MainService } from '../main.service';
   selector: 'app-character-details',
   template: `
   <button (click)="prev()">PREV</button> <button (click)="back()">BACK</button> <button (click)="next()">NEXT</button>
-    <p> Name: {{ content[charNumber].name }}
-      {{ content[charNumber].region }}
-    </p>
+    <p>Name: {{ content[charNumber].name }} ({{content[charNumber].type}} user)</p>
+    <p>Element: {{content[charNumber].element}}</p>
+    <p>Region: {{ content[charNumber].region }}</p>
+    <p>Talent Domain: {{ content[charNumber].domainname}}</p>
   `,
-  styles: [
+  styles: [``
   ]
 })
 export class CharacterDetailsComponent implements OnInit {
@@ -28,12 +29,12 @@ export class CharacterDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.charNumber = this.route.snapshot.params['id']
 
-    this._mainService.getCharacterView()
+    this._mainService.getAllCharacters()
     .subscribe(data => this.content = data);
 
     this.route.paramMap.subscribe((params:any)=>{
       let id = parseInt(params.get('id'));
-      this.charNumber = id;
+      this.charNumber = id - 1;
     });
   }
   
