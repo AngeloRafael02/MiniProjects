de665bf189daf57ad629f19a45659d311959717e
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-01 21:19:22 +0800

    removed Not Null Constraints

--- SQLScripts/NewGenshinImpactSQL/CreateAllTables.sql
+++ SQLScripts/NewGenshinImpactSQL/CreateAllTables.sql
@@ -4,69 +4,77 @@
 --Side Tables
 CREATE TABLE WeaponTypes (
     id SERIAL PRIMARY KEY,
-    Type VARCHAR(10) NOT NULL
+    Type VARCHAR(10)
 );
 CREATE TABLE Regions (
     id SERIAL PRIMARY KEY,
-    Region VARCHAR(20) NOT NULL
+    Region VARCHAR(20)
 );
 CREATE TABLE Elements (
     id SERIAL PRIMARY KEY,
-    Element VARCHAR(20) NOT NULL
+    Element VARCHAR(20)
 );
 CREATE TABLE SecondStats (
     id SERIAL PRIMARY KEY,
-    Stat VARCHAR(20) NOT NULL
+    Stat VARCHAR(20)
 );
 CREATE TABLE Availability (
     id SERIAL PRIMARY KEY,
-    Days VARCHAR(20)[] NOT NULL
+    Days VARCHAR(20)[]
 );
 CREATE TABLE AscensionDomains (
     id SERIAL PRIMARY KEY,
-    DomainName VARCHAR(40) NOT NULL,
-    Region_ID INT REFERENCES Regions(id) NOT NULL
+    DomainName VARCHAR(40),
+    Region_ID INT REFERENCES Regions(id)
+);
+CREATE TABLE SecondaryAscensionMaterials ( --Data to be added
+    id BIGSERIAL PRIMARY KEY,
+    Material VARCHAR(30)[],
+    Source VARCHAR(40)[]
 );
 CREATE TABLE CharacterDomainMaterials (
     id SERIAL PRIMARY KEY,
-    Material VARCHAR(20) NOT NULL,
-    Domain_ID INTEGER REFERENCES Domains(id) NOT NULL,
-    Availabilities INT REFERENCES Availability(id) NOT NULL
+    Material VARCHAR(20),
+    Domain_ID INTEGER REFERENCES AscensionDomains(id),
+    Availabilities INT REFERENCES Availability(id)
 );
 CREATE TABLE WeaponDomainMaterials (
     id SERIAL PRIMARY KEY,
-    Material VARCHAR(20) NOT NULL,
-    Domain_ID INTEGER REFERENCES Domains(id) NOT NULL,
-    Availabilities INT REFERENCES Availability(id) NOT NULL
+    Material VARCHAR(20),
+    Domain_ID INTEGER REFERENCES AscensionDomains(id),
+    Availabilities INT REFERENCES Availability(id)
 );
+
 
 --Main Tables
 CREATE TABLE Characters (
     id BIGSERIAL PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL,
-    Star INTEGER NOT NULL,
-    Element_ID BIGINT REFERENCES Elements(id) NOT NULL,
-    WeaponType_ID BIGINT REFERENCES WeaponTypes(id) NOT NULL,
-    Region_ID BIGINT REFERENCES Regions(id) NOT NULL,
-    Domain_ID BIGINT REFERENCES Domains(id) NOT NULL,
-    DomainMaterial_ID BIGINT REFERENCES CharacterDomainMaterials(id) NOT NULL,
-    Availabilties INT REFERENCES Availability(id) NOT NULL,
-    ImgURL TEXT NOT NULL
+    Name VARCHAR(50),
+    Star INTEGER,
+    Element_ID BIGINT REFERENCES Elements(id),
+    WeaponType_ID BIGINT REFERENCES WeaponTypes(id),
+    Region_ID BIGINT REFERENCES Regions(id),
+    Domain_ID BIGINT REFERENCES AscensionDomains(id),
+    DomainMaterial_ID BIGINT REFERENCES CharacterDomainMaterials(id),
+    --SecondaryAscensionMaterials_ID BIGINT REFERENCES WeaponDomainMaterials(id)
+    Availabilties INT REFERENCES Availability(id),
+    ImgURL TEXT
 );
 CREATE TABLE Weapons (
     id BIGSERIAL PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL,
-    Star INTEGER NOT NULL,
-    WeaponType_ID BIGINT REFERENCES WeaponTypes(id) NOT NULL,
-    SecondStat_ID BIGINT REFERENCES SecondStats(id NOT NULL),
-    Domain_ID BIGINT REFERENCES Domains(id) NOT NULL,
-    DomainMaterial_ID BIGINT REFERENCES WeaponDomainMaterials(id) NOT NULL,
-    Availabilties INT REFERENCES Availability(id) NOT NULL,
-    ImgURL TEXT NOT NULL
+    Name VARCHAR(50),
+    Star INTEGER,
+    WeaponType_ID BIGINT REFERENCES WeaponTypes(id),
+    SecondStat_ID BIGINT REFERENCES SecondStats(id) ,
+    Domain_ID BIGINT REFERENCES AscensionDomains(id),
+    DomainMaterial_ID BIGINT REFERENCES WeaponDomainMaterials(id),
+    --SecondaryAscensionMaterials_ID BIGINT REFERENCES WeaponDomainMaterials(id)
+    Availabilties INT REFERENCES Availability(id),
+    ImgURL TEXT
 );
 
 --ALTERATIONS for COLUMN and REFERENCES Adding
 
 ALTER TABLE Regions
-    ADD COLUMN Character_Domain_ID BIGINT REFERENCES Domains(id) NOT NULL, --reference only Character Domains, Make column NULLABLE
-    ADD COLUMN Weapon_Domain_ID BIGINT REFERENCES Domains(id) NOT NULL; --reference only weapon domains, Make column NULLABLE
+    ADD COLUMN Character_Domain_ID BIGINT REFERENCES AscensionDomains(id), --reference only Character Domains, Make column NULLABLE
+    ADD COLUMN Weapon_Domain_ID BIGINT REFERENCES AscensionDomains(id); --reference only weapon domains, Make column NULLABLE
