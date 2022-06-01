894658f52f07284d8de0ced67cdaa2576acd6f16
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-06-01 21:20:31 +0800

    Fixed the bugs made by separation of DomainMats  - Changed domain in createdViews.sql to the new tables

--- SQLScripts/NewGenshinImpactSQL/MainExe.sql
+++ SQLScripts/NewGenshinImpactSQL/MainExe.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE genshin_impact;
-\c genshin_impact
+CREATE DATABASE test4_db;
 \set ON_ERROR_STOP 1
 begin;
+    \c test4_db
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateAllTables.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/InsertSideTableData.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateCharactersData.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateWeaponsData.sql
     \i C:/Users/user/Documents/MiniProjects/SQLScripts/NewGenshinImpactSQL/CreateViews.sql
-commit;
+commit;\least
--- SQLScripts/NewGenshinImpactSQL/createViews.sql
+++ SQLScripts/NewGenshinImpactSQL/createViews.sql
@@ -1,49 +1,49 @@
 --CHARACTERS VIEWS
 --All Days Characters
 CREATE VIEW AllCharacters AS 
-SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, Domains.DomainName, DomainMaterials.Material, Availability.Days, ImgURL
+SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, AscensionDomains.DomainName, CharacterDomainMaterials.Material, Availability.Days, ImgURL
     FROM Characters
     JOIN Elements ON Characters.Element_id = Elements.id
     JOIN WeaponTypes ON Characters.WeaponType_id = WeaponTypes.id
     JOIN Regions ON Characters.Region_id = Regions.id
-    JOIN Domains ON Characters.Domain_id = Domains.id
-    JOIN DomainMaterials ON Characters.DomainMaterial_id = DomainMaterials.id
+    JOIN AscensionDomains  ON Characters.Domain_id = AscensionDomains .id
+    JOIN CharacterDomainMaterials ON Characters.DomainMaterial_id = CharacterDomainMaterials.id
     JOIN Availability ON Characters.Availabilties = Availability.id
     WHERE Availabilties IN (1,2,3)
     ORDER BY characters.id ASC;
 --Monday, Thursday, Sunday Characters
 CREATE VIEW Day1Characters AS 
-SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, Domains.DomainName, DomainMaterials.Material, Availability.Days, ImgURL
+SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, AscensionDomains.DomainName, CharacterDomainMaterials.Material, Availability.Days, ImgURL
     FROM Characters
     JOIN Elements ON Characters.Element_id = Elements.id
     JOIN WeaponTypes ON Characters.WeaponType_id = WeaponTypes.id
     JOIN Regions ON Characters.Region_id = Regions.id
-    JOIN Domains ON Characters.Domain_id = Domains.id
-    JOIN DomainMaterials ON Characters.DomainMaterial_id = DomainMaterials.id
+    JOIN AscensionDomains  ON Characters.Domain_id = AscensionDomains .id
+    JOIN CharacterDomainMaterials ON Characters.DomainMaterial_id = CharacterDomainMaterials.id
     JOIN Availability ON Characters.Availabilties = Availability.id
     WHERE Availabilties = 1 
     ORDER BY characters.id ASC;
 --Tuesday, Friday, Sunday Characters
 CREATE VIEW Day2Characters AS 
-SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, Domains.DomainName, DomainMaterials.Material, Availability.Days , ImgURL
+SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, AscensionDomains.DomainName, CharacterDomainMaterials.Material, Availability.Days , ImgURL
     FROM Characters
     JOIN Elements ON Characters.Element_id = Elements.id
     JOIN WeaponTypes ON Characters.WeaponType_id = WeaponTypes.id
     JOIN Regions ON Characters.Region_id = Regions.id
-    JOIN Domains ON Characters.Domain_id = Domains.id
-    JOIN DomainMaterials ON Characters.DomainMaterial_id = DomainMaterials.id
+    JOIN AscensionDomains  ON Characters.Domain_id = AscensionDomains .id
+    JOIN CharacterDomainMaterials ON Characters.DomainMaterial_id = CharacterDomainMaterials.id
     JOIN Availability ON Characters.Availabilties = Availability.id
     WHERE Availabilties = 2 
     ORDER BY characters.id ASC;
 --Wednesday, Saturday, Sunday Characters
 CREATE VIEW Day3Characters AS 
-SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, Domains.DomainName, DomainMaterials.Material, Availability.Days, ImgURL
+SELECT characters.id, name, star, Elements.Element, WeaponTypes.Type, Regions.Region, AscensionDomains.DomainName, CharacterDomainMaterials.Material, Availability.Days, ImgURL
     FROM Characters
     JOIN Elements ON Characters.Element_id = Elements.id
     JOIN WeaponTypes ON Characters.WeaponType_id = WeaponTypes.id
     JOIN Regions ON Characters.Region_id = Regions.id
-    JOIN Domains ON Characters.Domain_id = Domains.id
-    JOIN DomainMaterials ON Characters.DomainMaterial_id = DomainMaterials.id
+    JOIN AscensionDomains  ON Characters.Domain_id = AscensionDomains .id
+    JOIN CharacterDomainMaterials ON Characters.DomainMaterial_id = CharacterDomainMaterials.id
     JOIN Availability ON Characters.Availabilties = Availability.id
     WHERE Availabilties = 3
     ORDER BY characters.id ASC;
