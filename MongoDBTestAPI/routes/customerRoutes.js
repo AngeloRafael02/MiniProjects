29429039b75975e7e73bd5a8c5fce3b49dfda777
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-07-02 21:52:24 +0800

    moved db connection into a new file

--- MongoDBTestAPI/routes/customerRoutes.js
+++ MongoDBTestAPI/routes/customerRoutes.js
@@ -1,19 +1,12 @@
+const client = require('../db')
 const express = require('express');
-const customerRoute = express.Router(),
-      { MongoClient } = require("mongodb");
-
-const uri = "mongodb+srv://RecioAngeloRafael:XPWy77tpEFjHWuy@cluster0.cbzs3.mongodb.net/?retryWrites=true&w=majority"
-const client = new MongoClient(uri);
+const customerRoute = express.Router();
 
 customerRoute.get("/", async function (req,res) {
     try{
-        await client.connect();
-        const database = client.db('testDB');
-        const customers = database.collection('customers');
-        customers.find({}).toArray(function (err,result) {
+        client.collection.find({}).toArray(function (err,result) {
             if (err) throw err;
             res.json(result);
-            client.close();
         });
     } catch(error){console.log(error)}
 })
