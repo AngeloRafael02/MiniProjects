db45865d0584c22ed6131072b83cf328f336c016
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-19 20:02:51 +0800

    Added all CharacterRoute to index.js

--- NodeAndMongoDB/index.js
+++ NodeAndMongoDB/index.js
@@ -4,9 +4,11 @@ const app = express();
 
 const Router = require('./routes/CharacterRoutes').Router;
 const RegionRouter = require('./routes/CharacterRoutes').RegionRouter;
-
+const AllCharacters = require('./routes/CharacterRoutes').AllCharacters;
+ 
 app.use('/Characters', Router);
 app.use('/Regions', RegionRouter);
+app.use('/AllCharacters', AllCharacters);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
