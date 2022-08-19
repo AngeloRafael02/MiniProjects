9c4fa85cbeecb23a5b3239e7a1d9015161cda89c
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-19 19:58:21 +0800

    Added route for All Characters Query

--- NodeAndMongoDB/routes/CharacterRoutes.js
+++ NodeAndMongoDB/routes/CharacterRoutes.js
@@ -1,20 +1,19 @@
-const client = require('../db');
+const client = require('../db').client;
 const Router = require('express').Router();
 const RegionRouter = require('express').Router();
+const AllCharacters = require('express').Router();
 
-//NOTE: Fix the sorting of the characters by region
 Router.get('/:name', async (req, res) => {
     const requestName =  req.params.name
     try{
-        client.connect().then(async function(){
-            const response = await client.connect()
-            response.db("GI_DB").collection('Characters').find({name:requestName})
-                .toArray(function(err, result) {
-                    if (err) throw err;
-                    res.json(result);
-                }
-            );
-        });
+        const response = await client.connect()
+        response.db("GI_DB").collection('Characters')
+            .find({name:requestName},{_id:0}).project({_id:0})
+            .toArray(function(err, result) {
+                if (err) throw err;
+                res.json(result);
+            }
+        );
     }catch(err){
         console.log(err.stack);
     }
@@ -23,19 +22,35 @@ Router.get('/:name', async (req, res) => {
 RegionRouter.get('/:region', async (req, res) => {
     const requestRegion =  req.params.region
     try{
-        client.connect().then(async function(){
-            const response = await client.connect()
-            response.db("GI_DB").collection('Characters').find({region:requestRegion})
-                .toArray(function(err, result) {
-                    if (err) throw err;
-                    res.json(result);
-                }
-            );
-        });
+        const response = await client.connect()
+        response.db("GI_DB").collection('Characters')
+            .find({region:requestRegion},{_id:0}).project({_id:0})
+            .sort({star:1, name:1})
+            .toArray(function(err, result) {
+                if (err) throw err;
+                res.json(result);
+            }
+        );
     }catch(err){
         console.log(err.stack);
     }
 });
 
-module.exports = {Router, RegionRouter};
+AllCharacters.get('/', async (req, res) => {
+    try{
+        const response = await client.connect()
+        response.db("GI_DB").collection('Characters')
+            .find({},{_id:0}).project({_id:0})
+            .sort({region:1,star:1, name:1})
+            .toArray(function(err, result) {
+                if (err) throw err;
+                res.json(result);
+            }
+        );
+    }catch(err){
+        console.log(err.stack);
+    }
+})
+
+module.exports = {Router, RegionRouter,AllCharacters};
 
