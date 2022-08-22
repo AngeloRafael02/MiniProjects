69a0f4305245760737d34643e83dcb57aecf4a82
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-22 20:43:51 +0800

    Added GET all documents for Collection checking

--- MongooseJS/main.js
+++ MongooseJS/main.js
@@ -1,3 +1,6 @@
+const mongooseClient = require('./client').mongooseClient
+const User = require("./schema");
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express()
@@ -7,6 +10,14 @@ const testRoute = require('./routes/testRoutes')
 app.use(bodyParser.json())
 
 app.use("/test",testRoute)
+app.get('/all',async(req,res)=>{
+    try{
+        mongooseClient
+        const response = await User.find()
+        res.json(response)
+        console.log(response)
+    }catch(error){console.log(error)}  
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
