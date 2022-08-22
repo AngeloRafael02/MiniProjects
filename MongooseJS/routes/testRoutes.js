51d60f9e721e8bc6a9c2e5ced48b3599575403ee
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-22 20:43:26 +0800

    Added PATCH and DELETE request in testRoute

--- MongooseJS/routes/testRoutes.js
+++ MongooseJS/routes/testRoutes.js
@@ -3,10 +3,14 @@ const User = require("../schema");
 const mongooseClient = require('../client').mongooseClient
 
 testRoute.get("/:name", async(req,res)=>{
+    try{
+        mongooseClient
+        const response = await User.find({name:req.params.name})
+        res.json(response)
+        console.log(response)
+    }catch(error){console.log(error)}
     mongooseClient
-    const response = await User.find({name:req.params.name})
-    res.json(response)
-    console.log(response)
+    
 });
 
 testRoute.post("/", async (req,res)=>{
@@ -24,6 +28,33 @@ testRoute.post("/", async (req,res)=>{
     }
 });
 
-//NOTE:Make PATCH adn DELETE requests
+testRoute.patch('/:name', async (req,res)=>{
+    try{
+        mongooseClient
+        await User.findOneAndUpdate({name:req.params.name},{name:req.body.name}, function (err,doc) {
+                if (err) throw err;
+                console.log(doc);
+            });
+        res.send("Update Successful")
+    }catch(error){
+        console.log(error);
+    }
+});
+
+testRoute.delete('/:name', async (req,res)=>{
+    try{
+        mongooseClient
+        const nameQuery = req.params.name
+        await User.deleteOne({name:nameQuery},function(error){
+            if(error) {throw error}
+            else{ res.send(nameQuery + " has been Removed.")
+            console.log(nameQuery + " has been Removed.")}
+        })
+    }catch(error){
+        console.log(error);
+    }
+});
+
+//NOTE:fix any thrown errors
 
 module.exports = testRoute
