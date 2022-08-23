4b988c49b2f9a8f688f73047b459679bd58add84
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-23 20:23:44 +0800

    Cleaned the code and fixed the errors (more)     - used a better format for the routes     - removed callback functions on PATCH and DELETE requests

--- MongooseJS/routes/testRoutes.js
+++ MongooseJS/routes/testRoutes.js
@@ -2,58 +2,46 @@ const testRoute = require('express').Router();
 const User = require("../schema");
 const mongooseClient = require('../client').mongooseClient
 
-testRoute.get("/:name", async(req,res)=>{
-    try{
-        mongooseClient
-        const response = await User.find({name:req.params.name})
-        res.json(response)
-        console.log(response)
-    }catch(error){console.log(error)}
-    mongooseClient
-    
-});
-
-testRoute.post("/", async (req,res)=>{
-    try{
-        mongooseClient
-        const newUser = new User({
-            name:req.body.name,
-            age:req.body.age
-            })
-        await newUser.save()
-        res.json(newUser)
-        console.log(newUser.name + ' is saved')
-    }catch(error){
-        console.log(error)
-    }
-});
-
-testRoute.patch('/:name', async (req,res)=>{
-    try{
-        mongooseClient
-        await User.findOneAndUpdate({name:req.params.name},{name:req.body.name}, function (err,doc) {
-                if (err) throw err;
-                console.log(doc);
-            });
-        res.send("Update Successful")
-    }catch(error){
-        console.log(error);
-    }
-});
-
-testRoute.delete('/:name', async (req,res)=>{
-    try{
-        mongooseClient
-        const nameQuery = req.params.name
-        await User.deleteOne({name:nameQuery},function(error){
-            if(error) {throw error}
-            else{ res.send(nameQuery + " has been Removed.")
-            console.log(nameQuery + " has been Removed.")}
-        })
-    }catch(error){
-        console.log(error);
-    }
-});
+testRoute.route('/:name')
+    .get(async(req,res)=>{
+        try{
+            mongooseClient
+            const response = await User.find({name:req.params.name})
+            res.json(response)
+            console.log(response)
+        }catch(error){console.log(error)}    
+    })
+    .post(async(req,res)=>{
+        try{
+            mongooseClient
+            const newUser = new User({
+                name:req.params.name,
+                age:req.body.age
+                })
+            await newUser.save()
+            res.json(newUser)
+            console.log(newUser.name + ' is saved')
+        }catch(error){console.log(error)}
+    })
+    .patch(async(req,res)=>{
+        try{
+            mongooseClient
+            await User.findOneAndUpdate(
+                {name:req.params.name}, //filter
+                {name:req.body.name} //update
+                );
+            res.send("Update Successful")
+        }catch(error){console.log(error);}
+    })
+    .delete(async(req,res)=>{
+        try{
+            mongooseClient
+            const nameQuery = req.params.name
+            await User.deleteOne({name:nameQuery})
+            res.send(nameQuery + " has been Removed.")
+            console.log(nameQuery + " has been Removed.")
+        }catch(error){console.log(error);}
+    });
 
 //NOTE:fix any thrown errors
 
