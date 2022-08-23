a22248ec027ba5d11bd721111168376d6f233700
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-08-23 20:34:57 +0800

    initialized CRUD Route

---
+++ NodeAndMongoDB/routes/CRUDRoute.js
@@ -0,0 +1,17 @@
+const client = require('../db').client;
+const Router = require('express').Router();
+const CRUDRouter = require('express').Router();
+
+CRUDRouter.route("./:name")
+    .get(async(req,res)=>{
+
+    })
+    .post(async(req,res)=>{
+
+    })
+    .patch(async(req,res)=>{
+
+    })
+    .delete(async(req,res)=>{
+
+    });
