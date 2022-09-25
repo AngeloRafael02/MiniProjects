e6b2bff1fa411dab122f6958861345618c4c7069
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-09-25 21:30:43 +0800

    completed simple arithmetic Calculator

---
+++ Java/ArithmeticCalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ArithmeticCalculator {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input First Number: ");
+        float Input1 = input.nextFloat();
+        System.out.print("Input Second Number: ");
+        float Input2 = input.nextFloat();
+
+        Scanner StringInput = new Scanner(System.in);
+        System.out.println("Input Operation: ");
+        char operation = StringInput.next().charAt(0);
+        if((operation) == '+'){
+            System.out.println(Input1 + Input2);
+        } else if((operation) == '-'){
+            System.out.println(Input1 - Input2);
+        } else if((operation) == '*'){
+            System.out.println(Input1 - Input2);
+        } else if((operation) == '/'){
+            System.out.println(Input1 - Input2);
+        }else {
+            System.out.println("???");
+        }
+ 
+        input.close();
+        StringInput.close();
+    }
+}
