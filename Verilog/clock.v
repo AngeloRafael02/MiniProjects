3b24bd0a2526cc5526045a3495384d8ccdf0d194
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-11-19 19:29:36 +0800

    Added a clock sample

---
+++ Verilog/clock.v
@@ -0,0 +1,24 @@
+`timescale 1s/1ms
+
+module clockModule();
+    reg clk;
+    not(not1,clk);
+    and(and1,not1,clk);
+    or(or1,~clk,clk);
+    
+    initial
+        begin
+            $dumpfile("clock.vcd");
+	        $dumpvars(-1, clk);
+	        $monitor("%b : %b : %b : %b : time = %0t",clk,not1,and1,or1, $realtime);
+        end
+    initial 
+        begin
+        clk = 0;
+        forever 
+            #1 clk = ~clk;
+        end
+endmodule
+
+//NOTE: > make and and or gate display in gtkwave
+//      > find a way to implement clock in other modules/circuits
