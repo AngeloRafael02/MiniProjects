59271bb34d5b1e2d81335c5353e70c7e8069d4e4
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-12-16 14:29:59 +0800

    Added iteration count

--- AlgorithmsAndMath/binarySearchAlgorithm.py
+++ AlgorithmsAndMath/binarySearchAlgorithm.py
@@ -5,15 +5,19 @@ def binarySearch(array, x):
     # Repeat until the pointers low and high meet each other
     low = 0
     high = len(array)-1
-    
+    iteration = 0
     while low <= high:
         mid = low + (high - low)//2
         if array[mid] == x:
             return mid
         elif array[mid] < x:
             low = mid + 1
+            iteration +=1
+            print(iteration)
         else:
             high = mid - 1
+            iteration -=1
+            print(iteration)
     return -1
 
 array = [3, 4, 5, 6, 7, 8, 9]
